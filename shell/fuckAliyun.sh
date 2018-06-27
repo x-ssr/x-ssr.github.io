84d14bec4b05d79989bb8a9cabfd1f1b3565da28
admin <admin@x-ssr.com>
2018-06-27 11:40:12 +0800

    no message

---
+++ shell/fuckAliyun.sh
@@ -0,0 +1,9 @@
+wget http://update.aegis.aliyun.com/download/uninstall.sh
+chmod +x uninstall.sh
+./uninstall.sh
+wget http://update.aegis.aliyun.com/download/quartz_uninstall.sh
+chmod +x quartz_uninstall.sh
+./quartz_uninstall.sh
+pkill aliyun-service
+rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+rm -rf /usr/local/aegis*
